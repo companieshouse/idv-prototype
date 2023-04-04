b129f424b3270d276202babdd5a230310ec7f3a0
Emma <ejames@companieshouse.gov.uk>
2023-04-04 13:53:58 +0100

    bringing the screener to the start of the journey
    Name and company details added to heading caption

--- app/routes.js
+++ app/routes.js
@@ -6,17 +6,32 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Sign in
-//require('./routes/sign-in.js')(router)
-
-// Company lookup
-//require('./routes/company-lookup.js')(router)
-
-// register as an ACSP
-//require('./routes/add-journey.js')(router)
-
 
 /*
  * Register an ACSP 
  */
 
+
+
+// Corporate, firm or individual 
+router.post('register-an-acsp/after-idv', function (req, res) {
+    
+    if (req.session.data['registering-as'] === 'corporate') {
+        
+        res.redirect('company-lookup')
+    }
+    else if (req.session.data['registering-as'] === 'firm') {
+        
+        res.redirect('/register-an-acsp/acsp-name')
+    }
+    else if (req.session.data['registering-as'] === 'individual') {
+        
+        res.redirect('/register-an-acsp/acsp-name')
+    }
+    
+  }) 
+
+
+
+
+  module.exports = router
