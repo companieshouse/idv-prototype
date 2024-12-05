70af81de861044d3826246b2735e3b5630f536e0
Emma <ejames@companieshouse.gov.uk>
2024-12-05 08:17:29 +0000

    index page created

--- app/routes.js
+++ app/routes.js
@@ -7,22 +7,4 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 //registration
-router.use('/', require('./routes/routes-v1.js'))
-router.use('/', require('./routes/routes-v2.js'))
-router.use('/', require('./routes/routes-v3.js'))
-router.use('/', require('./routes/routes-v4.js'))
-router.use('/', require('./routes/routes-v5.js'))
-router.use('/', require('./routes/routes-v6.js'))
-router.use('/', require('./routes/routes-v7.js'))
-router.use('/', require('./routes/routes-v8.js'))
-router.use('/', require('./routes/routes-v9.js'))
-router.use('/', require('./routes/routes-v10.js'))
-router.use('/', require('./routes/routes-v11.js'))
-router.use('/', require('./routes/routes-v12.js'))
-router.use('/', require('./routes/routes-v13.js'))
 router.use('/', require('./routes/routes-v14.js'))
-
-//update 
-router.use('/', require('./routes/routes-update-v1.js'))
-router.use('/', require('./routes/routes-update-v2.js'))
-router.use('/', require('./routes/routes-update-v3.js'))
